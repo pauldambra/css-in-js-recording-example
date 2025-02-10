3504c8c5bed66020a708ec0b93fb282b35c7f6ae
Paul D'Ambra <paul@posthog.com>
2025-02-10 13:15:08 +0000

    with some mutations

--- src/DemoButton.tsx
+++ src/DemoButton.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function DemoButton() {
-  return <Button variant="contained">Hello world</Button>;
+  const [loading, setLoading] = React.useState(false);
+  const [clicks, setClicks] = React.useState(0);
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  });
+
+  return <Button variant="contained" loading={loading} endIcon={<SendIcon />} onClick={() => {
+    console.log('clicked');
+    setLoading(true);
+    setClicks(clicks + 1);
+  }}>Click me to cause some mutations on the page. Already clicked {clicks} times</Button>;
 }
